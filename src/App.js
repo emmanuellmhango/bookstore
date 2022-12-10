aa17af31950be433141d6a65c88bf1bc93575e67
Emmanuel <emmanuellmhango@gmail.com>
2022-12-10 14:37:13 +0400

    Clearing Routing errors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Books from './pages/Books';
 import ListBooks from './components/ListBooks';
 import Categories from './pages/Categories';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/books" element={<ListBooks />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<ListBooks />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  </>
+);
 
 export default App;
