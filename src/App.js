b05d08f67ba1b978001fea0f7496ee9264ad90e1
Emmanuel <emmanuellmhango@gmail.com>
2022-12-10 11:33:32 +0400

    Correct links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Books from './pages/Books';
 import ListBooks from './components/ListBooks';
 import Categories from './pages/Categories';
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
-        <Route path="/" element={<ListBooks />} />
+        <Route path="/" element={<Books />} />
+        <Route path="/books" element={<ListBooks />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
