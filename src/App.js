e2b32d38ccb75e5eb1289a8ec2b3b57e799d3485
Emmanuel <emmanuellmhango@gmail.com>
2022-12-10 09:41:27 +0400

    Edit Books component toclear linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Books from './pages/Books';
