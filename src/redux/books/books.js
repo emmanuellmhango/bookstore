f94eb0025bb104e1d1b374bbf1801318988086dd
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2022-12-14 22:23:15 +0400

    Fix review errors

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,5 @@
-const ADD_BOOK = 'bookstore/books/ADD_BOOK'
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK'
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 export const addBook = (book) => ({
   type: ADD_BOOK,
